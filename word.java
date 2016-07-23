83a46221db8b99b50de4941d9588679e8cc9e720
SindhuBE <sindhumol.erode@gmail.com>
2016-07-23 21:34:45 +0530

    Create word.java

---
+++ word.java
@@ -0,0 +1,19 @@
+
+public class Word 
+{
+
+	
+	public static void main(String[] args) 
+	{
+		System.out.println("Enter the string : ");
+		String s="Hello";
+		for(int i=0;i<5;i++)
+		{
+			System.out.println("Hello");
+		}
+		
+		
+
+	}
+
+}
