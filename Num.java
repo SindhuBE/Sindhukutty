a87fe255f8451c7a85a4ff86c19c1657abfb9114
SindhuBE <sindhumol.erode@gmail.com>
2016-07-23 21:42:23 +0530

    Create Num.java

---
+++ Num.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class Num {
+
+	
+	public static void main(String[] args)
+	{
+		Scanner s=new Scanner(System.in);
+
+
+		System.out.println("Enter the String: ");
+		String n1=s.nextLine();
+		int length=n1.length();
+		System.out.println("length is: " + +length);
+
+	}
+
+}
