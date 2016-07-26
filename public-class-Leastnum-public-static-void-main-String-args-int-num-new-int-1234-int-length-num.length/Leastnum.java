37c37281ccc7eacd6cf3e893e657baf81e04f582
SindhuBE <sindhumol.erode@gmail.com>
2016-07-26 14:40:54 +0530

    Create Leastnum.java

---
+++ public-class-Leastnum-public-static-void-main-String-args-int-num-new-int-1234-int-length-num.length/Leastnum.java
@@ -0,0 +1,25 @@
+
+public class Leastnum
+{
+	public static void main(String[] args)
+	{
+		int[] num=new int[]{1234};
+		int length=num.length;
+		//int []num=1234;
+		for(int i=0;i<num[i+9];i++)
+		{
+			if(num[i]>=num[i+1])
+			{
+				System.out.println("The smallest num is:"+num[i]);
+			}
+			else if(num[i]>=num[i+1])
+			{
+				System.out.println("The smallest num is :"+num[i]);
+			}
+		}
+		
+		
+
+	}
+
+}
