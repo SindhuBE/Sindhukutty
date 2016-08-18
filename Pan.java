8af3a55d77a052500cbe41a4eebb58f26049aace
SindhuBE <sindhumol.erode@gmail.com>
2016-08-18 15:53:51 +0530

    Create Pan.java

---
+++ Pan.java
@@ -0,0 +1,31 @@
+public class Pan{
+public static String Pana()
+{int flag=0;
+for(ch='a';ch<'z';ch++)
+{
+if(ch>26)
+{
+ System.out.println("Not a panuram");
+}
+else if(input.toLowerCase(String.valueOf(ch));
+{
+flag==1;
+
+}
+
+
+}if(flag==1)
+{
+System.out.println("panuram);
+}
+else 
+{
+System.out.println("Not panuram");
+}
+}
+public static void main(String [] args)
+{
+Pan p=new Pan();
+p.Pana("My name is sindhu");
+}
+}
